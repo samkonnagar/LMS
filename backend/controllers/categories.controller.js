78b901aedc4599fe7587d0357e03d93429bd346d
sam <samkonnagar123@gmail.com>
2025-07-07 16:36:19 +0530

    category controllers completed

--- backend/controllers/categories.controller.js
+++ backend/controllers/categories.controller.js
@@ -1,13 +1,80 @@
+import CategoryModal from "../models/Category.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const handleGetAllCategories = async (req, res) => {
-  res.json({ message: "Feature not added Yet!" });
+  let skip = 0;
+  let limit = 20;
+  if (req.query?.page) {
+    const getNum = Number(req.query.page);
+    if (isNaN(getNum) || getNum < 1) {
+      throw new ApiError(400, "Invalid Datatype");
+    }
+    skip = (getNum - 1) * limit;
+  }
+  const categories = await CategoryModal.find({})
+    .skip(skip)
+    .limit(limit)
+    .select("-__v");
+
+  if (!categories) {
+    throw new ApiError(500, "Something Wrong");
+  }
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      {
+        categories,
+      },
+      "Successfull"
+    )
+  );
 };
 
 const handleAddCategory = async (req, res) => {
-  res.json({ message: "Feature not added Yet!" });
+  if (!req.body?.categoryName) {
+    throw new ApiError(400, "Category name is required");
+  }
+
+  const { categoryName } = req.body;
+
+  // validation - not empty
+  if (categoryName.trim() === "") {
+    throw new ApiError(400, "Category name can't be empty");
+  }
+
+  // check if user category name exist
+  const existedCategory = await CategoryModal.findOne({
+    name: categoryName,
+  });
+
+  if (existedCategory) {
+    throw new ApiError(409, "Category already exist");
+  }
+
+  // create category
+  const category = await CategoryModal.create({
+    name: categoryName.trim(),
+  });
+
+  if (!category) {
+    throw new ApiError(500, "Something went wrong while creating category");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, { category }, "Category Created Successfully"));
 };
 
 const handleDeleteCategory = async (req, res) => {
-  res.json({ message: "Feature not added Yet!" });
+  const _id = req.params?.id;
+  const category = await CategoryModal.findByIdAndDelete(_id);
+  if (!category) {
+    throw new ApiError(400, "Invalid Id");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Category Deleted Successfully"));
 };
 
 export { handleGetAllCategories, handleAddCategory, handleDeleteCategory };
--- backend/routes/categories.route.js
+++ backend/routes/categories.route.js
@@ -4,11 +4,17 @@ import {
   handleAddCategory,
   handleDeleteCategory,
 } from "../controllers/categories.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import authorizeRole from "../middleware/role.middleware.js";
 
 const router = Router();
+router.use(verifyJWT);
 
-router.route("/").get(handleGetAllCategories).post(handleAddCategory);
+router
+  .route("/")
+  .get(handleGetAllCategories)
+  .post(authorizeRole("admin"), handleAddCategory);
 
-router.route("/:id").delete(handleDeleteCategory);
+router.route("/:id").delete(authorizeRole("admin"), handleDeleteCategory);
 
 export default router;
