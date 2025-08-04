0abbf06bda23a8714c892a9ab0fbf9e84c95e83e
Sam Roy <samkonnagar123@gmail.com>
2025-08-04 16:59:59 +0530

    implement course enrollment functionality with error handling and authorization

--- backend/controllers/enrollment.controller.js
+++ backend/controllers/enrollment.controller.js
@@ -1,5 +1,37 @@
+import mongoose from "mongoose";
+import CourseModel from "../models/Course.model.js";
+import EnrollmentModel from "../models/Enrollment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
 const handleEnrollCourse = async (req, res) => {
-  res.json({ message: "Feature not added Yet!" });
+  const courseId = req.params?.courseId;
+  if (!courseId) throw new ApiError(400, "Course ID is required");
+  const course = await CourseModel.findById(courseId);
+  if (!course) throw new ApiError(404, "Course not found");
+
+  const userId = req.user?._id;
+  const isBlocked = req.user?.isBlocked;
+  if (isBlocked)
+    throw new ApiError(403, "You are blocked from enrolling in courses");
+
+  const existingEnrollment = await EnrollmentModel.findOne({
+    student: new mongoose.Types.ObjectId(userId),
+    course: new mongoose.Types.ObjectId(courseId),
+  });
+  if (existingEnrollment)
+    throw new ApiError(400, "Already enrolled in this course");
+
+  const enrollment = await EnrollmentModel.insertOne({
+    student: userId,
+    course: courseId,
+  });
+
+  if (!enrollment) throw new ApiError(500, "Enrollment failed");
+
+  res
+    .status(201)
+    .json(new ApiResponse(201, enrollment, "Enrolled successfully"));
 };
 
 const handleGetAllCourses = async (req, res) => {
--- backend/routes/enrollments.route.js
+++ backend/routes/enrollments.route.js
@@ -6,8 +6,12 @@ import {
   markLessonCompleted,
   handleGetPdfCertificate,
 } from "../controllers/enrollment.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
+import authorizeRole from "../middleware/role.middleware.js";
 
 const router = Router();
+router.use(verifyJWT);
+router.use(authorizeRole("student"));
 
 router.route("/:courseId").post(handleEnrollCourse);
 router.route("/my-courses").get(handleGetAllCourses);
